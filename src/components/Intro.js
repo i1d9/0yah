2d4a5182469df2515b3ac7c1d754737e66146577
0yah <highmeme10@gmail.com>
2021-05-05 23:08:31 +0300

    Layout complete

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,23 @@
 export const Intro = () =>{
     return <div className="intro">
-        <div>
-            
+        <div className="fileBar">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <div className="content">
+            <div className="ProfileBrief">
+                
+            </div>
+            <div className="ProfileIntro">
+            <div className="profileImage">
+                    <img />
+                </div>
+            </div>
+            <div className="BackHeader">
+
+            </div>
         </div>
     </div>
 } 
