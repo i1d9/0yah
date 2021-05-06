5ed56d18f7a7f1488e69724ab89ea84e187f13df
0yah <highmeme10@gmail.com>
2021-05-06 19:18:00 +0300

    Added Profile Picture and profile svgs

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,3 +1,5 @@
+import dp from '../img/dp.jpg';
+import header from '../img/header.jpg';
 export const Intro = () =>{
     return <div className="intro">
         <div className="fileBar">
@@ -8,15 +10,39 @@ export const Intro = () =>{
 
         <div className="content">
             <div className="ProfileBrief">
-                
+                <div className="skills">
+                    <span>Expertise</span>
+                <ul>
+                    <li>React</li>
+                    <li>Express</li>
+                    <li>React Native</li>
+                    <li>MongoDB</li>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>GraphQL</li>
+                </ul>                    
+                </div>
+
             </div>
             <div className="ProfileIntro">
             <div className="profileImage">
-                    <img />
+                    <img src={dp} />
+                </div>
+                <div className="content">
+                    <span>Ian Naylan</span>
+                    <span>Computer Science Student</span>
+                    <span>Nairobi, Kenya</span>
+                    <div className="profiles">
+                    <i class="bi bi-github"></i>
+                    <i class="bi-alarm"></i>
+                    <i class="bi bi-twitter"></i>
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-whatsapp"></i>
+                    </div>
                 </div>
             </div>
             <div className="BackHeader">
-
+                
             </div>
         </div>
     </div>
